b06df26da66bb35f6ec464cd237578aaacce95ac
beel <hpit.vie@gmail.com>
2025-03-28 11:13:58 +0700

    - change apis permissions

--- app/api/api_v1/routers/exchanges.py
+++ app/api/api_v1/routers/exchanges.py
@@ -16,7 +16,7 @@ def read_exchanges(
     limit: int = 100,
     current_user: models.User = Security(
         deps.get_current_active_user,
-        scopes=[Role.ADMIN["name"], Role.SUPER_ADMIN["name"]],
+        scopes=[Role.ADMIN["name"], Role.SUPER_ADMIN["name"], Role.USER["name"]],
     ),
 ) -> Any:
     """
